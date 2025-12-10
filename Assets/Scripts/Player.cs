c869ea36e0c368beea72637b27d10fc1f0380753
God-Boss <Santiagosaenz86@gmail.com>
2025-12-10 00:39:21 +0000

    Scrip player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public Rigidbody2D ribiBody2D;
+
+    private float inputValue;
+
+    public float moveSpeed = 25;
+
+    private Vector2 direction;
+
+
+
+
+
+    
+    
+    void Update()
+    {
+        inputValue = Input.GetAxisRaw("Horizontal");
+
+        if (inputValue == 1)
+        {
+            direction = Vector2.right;
+        }
+        else if (inputValue == -1)
+        {
+            direction = Vector2.left;
+        }
+        else
+        {
+            direction = Vector2.zero;
+        }
+
+        ribiBody2D.AddForce(direction * moveSpeed * Time.deltaTime * 100);
+        
+        
+    }
+}
